e46cc6d0da5999fafe5dbec293c072eaa6b92f21
Kranthi <cascade.kiran.2010@gmail.com>
2018-02-14 22:27:40 +0530

    Changed config.js file

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 var env = process.env.NODE_ENV || 'development';
 
-if(env === 'development' || env ==== 'test'){
+if(env === 'development' || env === 'test'){
 
   var configFile = require('config.json');
 
